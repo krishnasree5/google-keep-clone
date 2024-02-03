3b72561768bc42f2f5c7e17329bf6bcb4456b6ba
{Krishna <gkrishnasree5@gmail.com>
2024-02-03 18:12:19 +0530

    Finish website template creation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Keep</h1>
+        </div>
+    );
+}
+
+const Main = () => {
+    return(
+        <div className='container'>
+            <div className='item'>
+                <h1>Heading</h1>
+                <p>Sample text goes on and on and on.</p>
+            </div>
+        </div>
+    );
+}
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+        <div className='footer'>
+            <p>Copyright &copy; {year}</p>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
+    );
+}
+
+root.render(<App/>);
