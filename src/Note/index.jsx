1121199eae4fd78a0fec85dc439fd5ca7853a5e4
{Krishna <gkrishnasree5@gmail.com>
2024-06-24 19:44:31 +0530

    Notes

---
+++ src/Note/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Note = ({title, description}) => {
+
+    return (
+        <div className='note'>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Main from "./Main";
+import Footer from './Footer';
+import Header from './Header';
+
 import './index.css';
+import notes from './notes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Header = () => {
-    return (
-        <div className='header'>
-            <h1>Keep</h1>
-        </div>
-    );
-}
-
-const Main = () => {
-    return(
-        <div className='container'>
-            <div className='item'>
-                <h1>Heading</h1>
-                <p>Sample text goes on and on and on.</p>
-            </div>
-        </div>
-    );
-}
-
-const Footer = () => {
-    const year = new Date().getFullYear();
-    return (
-        <div className='footer'>
-            <p>Copyright &copy; {year}</p>
-        </div>
-    );
-}
-
 const App = () => {
     return (
-        <>
+        <div>
             <Header/>
-            <Main/>
+            <Main notes={notes} />
             <Footer/>
-        </>
+        </div>
     );
 }
 
